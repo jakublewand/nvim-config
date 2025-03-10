d7e21c496bdcb6a29392509880e9e925718b8a97
Jakub Lewandowski <jakub.lew@icloud.com>
2025-03-10 22:10:23 +0100

    Added theme and lsps

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+require("config.lazy")
+require("remaps")
+vim.cmd.colorscheme("darkplus")
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
---
+++ lua/plugins/darkplus.lua
@@ -0,0 +1,4 @@
+return {
+    "martinsione/darkplus.nvim",
+    lazy = false,
+  }
